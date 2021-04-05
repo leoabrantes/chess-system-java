1ee8ddb32ff14b5f25b75c83ecde0f779249eae7
leoabrantes <leoabrantesmelo@gmail.com>
2021-04-05 18:01:24 -0300

    Placing pieces on the board

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,10 +7,9 @@ import chess.ChessMatch;
 public class Program {
 
 	public static void main(String[] args) {
-		
+	
 	ChessMatch chessMatch = new ChessMatch();
 	UI.printBoard(chessMatch.getPieces());
-	
 		
 	}
 
