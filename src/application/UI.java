6c4684de201746e734d0ac6aa753d2fe86ccba72
Leo Abrantes <leoabrantesmelo@gmail.com>
2022-01-09 07:53:35 -0300

    Possible moves of a piece

--- src/application/UI.java
+++ src/application/UI.java
@@ -68,7 +68,7 @@ public class UI {
 			System.out.print("-" + ANSI_RESET);
 		} else {
 			if (piece.getColor() == Color.WHITE) {
-				System.out.print(ANSI_RED + piece + ANSI_RESET);
+				System.out.print(ANSI_WHITE + piece + ANSI_RESET);
 			}
 
 			else {
